78512f78e7413093314131a798c64ce9f72ddefa
kimhw7 <khw786678@gmail.com>
2023-05-08 11:55:19 +0900

    css 속성 적용
    MealItem css 속성 적용

--- src/components/Meals/MealItem/MealItem.tsx
+++ src/components/Meals/MealItem/MealItem.tsx
@@ -5,9 +5,9 @@ import type { DummyMeal } from "../AvailableMeals";
 const MealsItem = ({ props }: { props: DummyMeal }) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
-    <li className={classes.props}>
+    <li className={classes.meal}>
       <div>
-        <h3 className={classes.name}>{props.name}</h3>
+        <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
