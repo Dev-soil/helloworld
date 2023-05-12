79210a059d7cc49c0c5b040ed434d48aa856665f
kimhw7 <khw786678@gmail.com>
2023-05-12 19:45:03 +0900

    cart 버튼 여러번 클릭 시 한번만 애니메이션 재생
    useEffect cleanup 함수로 여러번 클릭시 settimeout을 통한 애니메이션 한번 재생'

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,19 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 import CartContext from "../../store/cartContext";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props: { onClick: () => void }) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfItems = cartCtx.items.reduce((cur, item) => {
     return cur + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
     <>
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <AiOutlineShoppingCart />
         </span>
